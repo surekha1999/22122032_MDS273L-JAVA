77b9c958bc520b78c319fca2d9bd63127100ee27
surekha1999 <118045110+surekha1999@users.noreply.github.com>
2023-05-08 09:33:00 +0530

    Add files via upload

---
+++ README lab7.java
@@ -0,0 +1,8 @@
+
+
+
+//Given the famous iris dataset, find the 5-point summary [Mean, Median, Mode, Min, Max] for the attributes: SepalLengthCm, SepalWidthCm, PetalLengthCm, PetalWidthCm.
+
+//Once the overall summary statistics have been calculated, identify the summary statistics for each Species of iris flower [Iris-setosa, Iris-versicolor, Iris-virginica].
+
+//Present your results in the appropriate format and write the results in a file.
